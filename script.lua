2e8cf79c5a6fc393e700af217e4c9654a95cbb2f
UnkownV_1 <blyfatimah599@gmail.com>
2026-09-20 12:29:45 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,7 +4,7 @@ local ProtectionConfig = {
     -- 🔴 CRITICAL: This MUST exactly match the 'Secret' value in your Key System's Config!
     -- If your Key System has: Secret = "Test"
     -- Then this must also be: SecretKey = "Test"
-    SecretKey = "1234",
+    SecretKey = "01927471946",
     
     -- The name of your Hub (shown in the kick message if they try to bypass)
     HubName = "X GUI HUB"
